e4a9c3c344851604b760619029d2ed984afeb5cf
Aman Sharma <aman703jk@gmail.com>
2022-09-03 14:49:47 +0530

    Initial Setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text className='text-red-500'>HomeScreen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
